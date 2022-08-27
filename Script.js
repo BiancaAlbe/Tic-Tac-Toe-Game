733378320097fa90851e057678b935af5ba922bf
Bianca <biabiancaalbertina@yahoo.com>
2022-08-27 14:29:32 +0300

    create a new version of tic-tac-toe

--- Script.js
+++ Script.js
@@ -1,85 +1,126 @@
 window.onload = function gridGenerate() {
-    for (var i = 1; i <= 3; ++i) {
-        for (var j = 1; j <= 3; ++j) {
-            document.getElementById('test1').innerHTML += "<div id=table1(" + i + "," + j + ") onclick = Clicks("+ i +","+ j +") class = click ></div>";
+    for (var i = 0; i < 3; ++i) {
+        for (var j = 0; j < 3; ++j) {
+            document.getElementById('test1').innerHTML += "<div id=table1(" + i + "," + j + ") onclick = table_clicks("+ i +","+ j +") class = click ></div>";
         }
         document.getElementById('test1').innerHTML += '<br>';
     }
     
 }
 
-let verify = 0, nrx1 = 0, nrx2 = 0, nrx3 = 0, nro1 = 0, nro2 = 0, nro3 = 0, nrj1 = 0, nrj2 = 0, nrj3 = 0;
-let nrdiagonal_x_p = 0, nrdiagonal_x_s = 0, nrdiagonal_o_p = 0, nrdiagonal_o_s = 0, nri1 = 0, nri2 = 0, nri3 = 0;
-stop_v = 0;
 
-function Clicks(i,j) {
-    if (stop_v === 1) {
-        return;
-    }
+let verify = 0;
+var square = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ];
+
+
+function table_clicks(i, j) {
+
     let z = document.getElementById('table1('+i+","+j+')').innerText;
     if (verify === 0 && z != 'X' && z != 'O') {
-        document.getElementById('table1('+i+","+j+')').innerHTML= 'X';
         verify = 1;
-        if (i === 1) {
-            ++nrx1;
-        }
-        if (i === 2) {
-            ++nrx2;
-        }
-        if (i === 3) {
-            ++nrx3;
-        }
-        if (i === j) {
-            ++nrdiagonal_x_p;
-        }
-        if (i + j === 4) {
-            ++nrdiagonal_x_s;
-        }
-        if (j === 1) {
-            ++nrj1;
-        }
-        if (j === 2) {
-            ++nrj2;
-        }
-        if (j === 3) {
-            ++nrj3;
+        document.getElementById('table1('+i+","+j+')').innerHTML= 'X';
+        square[i][j] = "x";
+    } else if (verify === 1 && z != 'X' && z != 'O') {
+            document.getElementById('table1('+i+","+j+')').innerHTML= 'O';
+            verify = 0;
+            square[i][j] = "o";
+    }
+    let afisare = array_verify(square);
+    console.log (afisare);
+    if (afisare) {
+        document.getElementById('mesage').innerText = afisare;
+    }
+}
+
+function array_verify(square) {
+        
+    let count_row = 0;
+    let count_column = 0;
+
+    for (let i = 0; i < 3; ++i) {
+        count_row = 0;
+        count_column = 0;
+
+        for (let j = 0; j < 3; ++j) {
+
+            if (square[i][j] === "x") {
+                ++count_row;
+            }
+            if (square[j][i] === "x") {
+                ++count_column;
+            }
+
         }
 
-    } else if (verify === 1 && z != 'X' && z != 'O') {
-        document.getElementById('table1('+i+","+j+')').innerHTML= 'O';
-        verify = 0;
-        if (j === 1) {
-            ++nro1;
-        }
-        if (j === 2) {
-            ++nro2;
-        }
-        if (j === 3) {
-            ++nro3;
-        }
-        if (j === i) {
-            ++nrdiagonal_o_p;
-        }
-        if (j + i === 4) {
-            ++nrdiagonal_o_s;
-        }
-        if (i === 1) {
-            ++nri1;
-        }
-        if (i === 2) {
-            ++nri2;
-        }
-        if (i === 3) {
-            ++nri3;
+        if (count_column === 3 || count_row === 3) {
+            return "X - you have won";
         }
     }
-    
-    if (nrx1 === 3 || nrx2 === 3 || nrx3 === 3 || nrdiagonal_x_p === 3 || nrdiagonal_x_s === 3 || nrj1 === 3 || nrj2 === 3 || nrj3 === 3) {
-        document.getElementById('afisare').innerText = ' X - You have won';
-        stop_v = 1;
-    } 
-    if (nro1 === 3 || nro2 === 3 || nro3 === 3 || nrdiagonal_o_p === 3 || nrdiagonal_o_s === 3 || nri1 === 3 || nri2 === 3 || nri3 === 3) {
-        document.getElementById('afisare').innerText = ' O - You have won';
-        stop_v = 1; 
+
+    for (let i = 0; i < 3; ++i) {
+        count_row = 0;
+        count_column = 0;
+
+        for (let j = 0; j < 3; ++j) {
+
+            if (square[i][j] === "o") {
+                ++count_row;
+            }
+            if (square[j][i] === "o") {
+                ++count_column;
+            }
+
+        }
+
+        if (count_row === 3 || count_column === 3) {
+            return "O - you have won";
+        }
     }
+
+    let main_diagonal_x = 0;
+    let main_diagonal_o = 0;
+
+    for (let i = 0; i < 3; ++i) {
+
+        if ( square[i][i] === "x") {
+            ++main_diagonal_x;
+        }
+        if (main_diagonal_x === 3) {
+            return "X - you have won";
+        }
+
+        if ( square[i][i] === "o") {
+            ++main_diagonal_o;
+        }
+        if (main_diagonal_o === 3) {
+            return "O - you have won";
+        }
+    }
+
+    let secondary_diagonal_x = 0;
+    let secondary_diagonal_o = 0;
+
+    for (let i = 0, j = 2; i <= 2, j >= 0; ++i, --j) {
+
+        if (square[i][j] === "x"){
+            ++secondary_diagonal_x;
+                
+            } 
+            if (secondary_diagonal_x === 3) {
+                return "X - you have won";
+            }
+
+            if (square[i][j] === "o"){
+                ++secondary_diagonal_o;
+            } 
+            if (secondary_diagonal_o === 3) {
+                return "O - you have won";
+            } 
+        
+    }
+       return false;
 }
