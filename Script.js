456548585d9bbbd4d714614db8ef2c958371dd2f
Bianca <biabiancaalbertina@yahoo.com>
2022-08-31 20:13:35 +0300

    showing the output in every box

--- Script.js
+++ Script.js
@@ -1,18 +1,10 @@
-window.onload = function gridGenerate() {
+window.onload = function gridGenerate() {                 // create the table
   for (var i = 0; i < 3; ++i) {
     for (var j = 0; j < 3; ++j) {
-      document.getElementById("test1").innerHTML +=
-        "<div id=table1(" +
-        i +
-        "," +
-        j +
-        ") onclick = table_clicks(" +
-        i +
-        "," +
-        j +
-        ") class = click ></div>";
+      document.getElementById("create_table").innerHTML +=
+        "<div id ='table" + i + " " + j + "' onclick = table_clicks(" + i + "," + j + ") class = click ></div>";
     }
-    document.getElementById("test1").innerHTML += "<br>";
+    document.getElementById("create_table").innerHTML += "<br>";
   }
 };
 
@@ -23,25 +15,24 @@ var square = [
   ["", "", ""],
 ];
 
-function table_clicks(i, j) {
-  let z = document.getElementById("table1(" + i + "," + j + ")").innerText;
-  if (verify === 0 && z != "X" && z != "O") {
+function table_clicks(i, j) {                            //add x or o in the box
+  let z = document.getElementById("table" + i + " " + j).innerHTML;
+  if (verify === 0 && z != "O" && z != "X") {
     verify = 1;
-    document.getElementById("table1(" + i + "," + j + ")").innerHTML = "X";
-    square[i][j] = "x";
-  } else if (verify === 1 && z != "X" && z != "O") {
-    document.getElementById("table1(" + i + "," + j + ")").innerHTML = "O";
-    verify = 0;
-    square[i][j] = "o";
+    document.getElementById("table" + i + " " + j).innerHTML = "X";
+    square[i][j] = "X";
   }
+  add_o_to_the_first_emoty_box(square);
+  console.log(square);
+
   let result = array_verify(square);
   console.log(result);
   if (result) {
-    document.getElementById("mesage").innerText = result;
+    document.getElementById("mesage").innerText = result; //shows the winner on the screen
   }
 }
 
-function array_verify(square) {
+function array_verify(square) {                           //check if there is a winner
   let count_row = 0;
   let count_column = 0;
 
@@ -50,10 +41,10 @@ function array_verify(square) {
     count_column = 0;
 
     for (let j = 0; j < 3; ++j) {
-      if (square[i][j] === "x") {
+      if (square[i][j] === "X") {
         ++count_row;
       }
-      if (square[j][i] === "x") {
+      if (square[j][i] === "X") {
         ++count_column;
       }
     }
@@ -68,10 +59,10 @@ function array_verify(square) {
     count_column = 0;
 
     for (let j = 0; j < 3; ++j) {
-      if (square[i][j] === "o") {
+      if (square[i][j] === "O") {
         ++count_row;
       }
-      if (square[j][i] === "o") {
+      if (square[j][i] === "O") {
         ++count_column;
       }
     }
@@ -85,14 +76,14 @@ function array_verify(square) {
   let main_diagonal_o = 0;
 
   for (let i = 0; i < 3; ++i) {
-    if (square[i][i] === "x") {
+    if (square[i][i] === "X") {
       ++main_diagonal_x;
     }
     if (main_diagonal_x === 3) {
       return "X - you have won";
     }
 
-    if (square[i][i] === "o") {
+    if (square[i][i] === "O") {
       ++main_diagonal_o;
     }
     if (main_diagonal_o === 3) {
@@ -104,14 +95,14 @@ function array_verify(square) {
   let secondary_diagonal_o = 0;
 
   for (let i = 0, j = 2; i <= 2, j >= 0; ++i, --j) {
-    if (square[i][j] === "x") {
+    if (square[i][j] === "X") {
       ++secondary_diagonal_x;
     }
     if (secondary_diagonal_x === 3) {
       return "X - you have won";
     }
 
-    if (square[i][j] === "o") {
+    if (square[i][j] === "O") {
       ++secondary_diagonal_o;
     }
     if (secondary_diagonal_o === 3) {
@@ -119,4 +110,18 @@ function array_verify(square) {
     }
   }
   return false;
+}
+
+function add_o_to_the_first_emoty_box(square) {              // add o in the first empty box
+
+  for (let i = 0; i < 3; ++i) {
+    for (let j = 0; j < 3; ++j) {
+      if (square[i][j] === "") {
+        square[i][j] = "O";
+        document.getElementById("table" + i + " " + j).innerHTML = square[i][j];
+        verify = 0;
+        return;
+      }
+    }
+  }
 }
